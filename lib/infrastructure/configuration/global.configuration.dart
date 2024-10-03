6b6c235cf1e49b44d670e72141d7696de6be4736
andres2508 <jaime.aristizabal.2508@gmail.com>
2024-10-03 15:53:03 -0500

    Fix, query global configuration from file

--- lib/infrastructure/configuration/global.configuration.dart
+++ lib/infrastructure/configuration/global.configuration.dart
@@ -1,5 +1,9 @@
 class GlobalCIATConfiguration {
   // Backend configuration
+  final String _redirectAuthURL = const String.fromEnvironment('redirect_url');
+  final String _genesysURL = const String.fromEnvironment('genesys_url');
+  final String _clientId = const String.fromEnvironment('client_id');
+  final String _clientSecretId = const String.fromEnvironment('client_secret');
   final String _server = const String.fromEnvironment('server');
 
   static final GlobalCIATConfiguration controller =
@@ -11,5 +15,17 @@ class GlobalCIATConfiguration {
     return controller;
   }
 
-  get server => _server;
+  String get apiServer => '$_server/api/v1';
+
+  String get authServer => '$_server/oauth2';
+
+  String get redirectAuthURL => _redirectAuthURL;
+
+  String get server => _server;
+
+  String get clientId => _clientId;
+
+  String? get clientSecretId => _clientSecretId;
+
+  String get genesysURL => _genesysURL;
 }
