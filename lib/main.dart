8e67bfe414891159376168856a640fe8b048d1a7
andres2508 <jaime.aristizabal.2508@gmail.com>
2024-10-11 08:08:02 -0500

    Bug, setup service locator not initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:base_project/app.dart';
 import 'package:base_project/framework/router/mubon.module.dart';
+import 'package:base_project/infrastructure/locator/service.locator.dart';
 import 'package:flutter/material.dart';
 
 List<CIATModule> modules = [];
 
 void main() {
+  setupServiceLocator();
   runApp(CIATMobileApp());
 }
